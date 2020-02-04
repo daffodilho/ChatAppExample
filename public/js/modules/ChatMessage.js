79f4b31fda6bc98a5a5c4a417f340732df2ab522
Daffodil Ho <daffodilho@Daffodils-MacBook-Pro.local>
2020-02-04 15:28:38 -0500

    making changes to speech bubbles

--- public/js/modules/ChatMessage.js
+++ public/js/modules/ChatMessage.js
@@ -5,7 +5,7 @@ export default {
     props: ['msg'],
 
     template: `
-        <p class="new-message">
+        <p class="new-message" :class="{ 'my-message' : matchedID }">
             <span>{{msg.message.name}} says:</span>
             {{msg.message.content}}
         </p>
@@ -13,6 +13,8 @@ export default {
 
     data: function() {
         // nothing here yet, but there will be
-        return { message: "it doesnt matter" }
+        return { 
+            matchedID: this.$parent.socketID == this.msg.id 
+        }
     }
 }
